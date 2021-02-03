71168ddf7c7791da5d1ac081a19057b1db6b04b6
wobey000 <wobey@uncc.edu>
2021-02-02 23:22:08 -0500

    Local test

--- ex1.c
+++ ex1.c
@@ -4,10 +4,10 @@
 int main(int argc, char *argv[])
 {
     int distance = 100;
-
+    // test from local amchine to github
     // this is also a comment
     printf("You are %d miles away\n", distance);
-    printf("My name is Wally world\n"); 
+    printf("My name is Wally world\n");
 
     return 0;
 }

26c09191989d6b0b8d2c4f2a4d4789765dbf71c7
wobey000 <wobey@uncc.edu>
2021-02-02 23:30:32 -0500

    Spell check

--- ex1.c
+++ ex1.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     int distance = 100;
-    // test from local amchine to github
+    // test from local mchine to github
     // this is also a comment
     printf("You are %d miles away\n", distance);
     printf("My name is Wally world\n");

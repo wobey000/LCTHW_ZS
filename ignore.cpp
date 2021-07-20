65b83b3906963d6a4b1c97a502f390071e54ddbb
wobey000 <wobey96@gmail.com>
2021-07-19 22:28:02 -0600

    added ignore.cpp, a dummy file :p

---
+++ ignore.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  int x = 8;
+  int y = 6;
+  std::cout << std::endl;
+  std::cout << x - y << " " << x * y << " " << x + y;
+  std::cout << std::endl;
+  return 0;
+}
+
+// git test
